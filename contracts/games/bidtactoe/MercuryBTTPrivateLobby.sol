0591d68a3dbf387f5c7cb775c1e014edbbbfb593
Skylab Dev <dev@skylab.wtf>
2023-12-11 01:58:10 -0800

    Introduce planeless and Private Lobby fixes

--- contracts/games/bidtactoe/MercuryBTTPrivateLobby.sol
+++ contracts/games/bidtactoe/MercuryBTTPrivateLobby.sol
@@ -2,16 +2,10 @@
 pragma solidity ^0.8.0;
 
 import "./MercuryBidTacToe.sol";
-import {MercuryGameBase} from "../base/MercuryGameBase.sol";
+import "../../aviation/base/MercuryBase.sol";
+import {LibBidTacToe} from "./lib/LibBidTacToe.sol";
 
-interface IPrivateLobbyFactory {
-    function clean(string memory) external;
-    function setHasJoined(string memory name, bool joined) external;
-    function hasJoined(address) external view returns (bool);
-}
-
-contract MercuryBTTPrivateLobby is MercuryGameBase {
-
+contract MercuryBTTPrivateLobby {
     struct GameHistory {
         address winnerBurner;
         address loserBurner;
@@ -19,143 +13,128 @@ contract MercuryBTTPrivateLobby is MercuryGameBase {
 
     struct UserInfo {
         uint256 avatar;
-        string  name;
+        string name;
     }
 
     // Dynamic game data
-    mapping(address => bool) private gameExists;
-    mapping(address => address) public gamePerPlayer;
-    mapping(address => MercuryBidTacToe.GameParams) public paramsPerGame;
-    mapping(address => MercuryBidTacToe.PlaneMetadata) public planeMetadataPerGame;
     mapping(address => UserInfo) public userInfo;
-
-    // aviation to lobbies game queue
-    mapping(address => address[]) lobbyGameQueue;
-    mapping(address => uint256) lobbyGameIndex;
-    mapping(address => address[]) lobbyOnGoingGames;
-    mapping(address => uint256) lobbyOnGoingGamesIndex;
-
-    //room related
-    address public factory;
     address[] public players;
     GameHistory[] public gameHistory;
-    address mercuryBidTacToe;
-    string lobbyName;
+    mapping(address => uint256) public winCountPerPlayer;
+    mapping(address => uint256) public loseCountPerPlayer;
 
-    constructor(address _mercuryBidTacToe, string memory _lobbyName) {
-        factory = msg.sender;
-        lobbyName = _lobbyName;
-        mercuryBidTacToe = _mercuryBidTacToe;
+    // aviation to lobbies game queue
+    address[] lobbyGameQueue;
+    mapping(address => uint256) lobbyGameIndex;
+    address[] lobbyOnGoingGames;
+    mapping(address => uint256) lobbyOnGoingGamesIndex;
 
+    // Static data
+    MercuryBidTacToe mercuryBidTacToe;
+    address public lobbyAviation;
+    address public admin;
+    string public lobbyName;
+
+    modifier isActiveLobbyAndCorrectAviation() {
+        require(
+            mercuryBidTacToe.activeLobbyPerPlayer(msg.sender) == address(this),
+            "MercuryBTTPrivateLobby: not active lobby"
+        );
+        require(
+            mercuryBidTacToe.burnerAddressToAviation(msg.sender) == lobbyAviation,
+            "MercuryBTTPrivateLobby: player aviation is not the same as lobby aviation"
+        );
+        _;
     }
 
-    function setUserInfo(uint256 avatar, string memory userName) public {
-        require(avatar >= 1 && avatar <= 16, "Private Lobby: avatar out of range");
-        require(bytes(userName).length <= 10, "Private Lobby: name too long");
+    constructor(string memory _lobbyName, address _admin) {
+        lobbyName = _lobbyName;
+        mercuryBidTacToe = MercuryBidTacToe(msg.sender);
+        admin = _admin;
+        lobbyAviation = mercuryBidTacToe.burnerAddressToAviation(admin);
+
+        joinPrivateLobby();
+    }
+
+    function setUserInfo(uint256 avatar, string memory userName) public isActiveLobbyAndCorrectAviation {
+        require(avatar >= 1 && avatar <= 16, "MercuryBTTPrivateLobby: avatar out of range");
+        require(bytes(userName).length <= 10, "MercuryBTTPrivateLobby: name too long");
         userInfo[msg.sender] = UserInfo(avatar, userName);
     }
 
-    function createLobby(MercuryBidTacToe.GameParams memory gameParams) external returns (address) {
-        address newGame = LibBidTacToe.createGame(gameParams, msg.sender, address(this));
-        address aviation = burnerAddressToAviation(msg.sender);
-        paramsPerGame[newGame] = gameParams;
-        planeMetadataPerGame[newGame] =
-            MercuryBidTacToe.PlaneMetadata(getAviationLevel(msg.sender, aviation), getAviationPoints(msg.sender, aviation), 0, 0);
-        gameExists[newGame] = true;
-        gamePerPlayer[msg.sender] = newGame;
-        baseCreateRoom(newGame, aviation);
+    function createRoom(MercuryBidTacToe.GameParams memory gameParams)
+        external
+        isActiveLobbyAndCorrectAviation
+        returns (address)
+    {
+        address newGame = mercuryBidTacToe.createGameInPrivateLobby(gameParams);
+        baseCreateRoom(newGame);
         return newGame;
     }
 
-    function joinLobby(address gameAddress, address player2) external {
-        require(gameExists[gameAddress], "PrivateLobby: lobby does not exist");
-        require(gamePerPlayer[player2] == address(0), "PrivateLobby: player already created or queued for a game");
+    function joinRoom(address gameAddress, address player2) external isActiveLobbyAndCorrectAviation {
+        mercuryBidTacToe.joinGameInPrivateLobby(gameAddress, player2);
+        baseJoinRoom(gameAddress);
+    }
+
+    function deleteRoom(address room) external isActiveLobbyAndCorrectAviation {
         require(
-            isIdenticalAviation(LibBidTacToe.getPlayer1(gameAddress), player2),
-            "PrivateLobby: aviation does not match"
+            msg.sender == LibBidTacToe.getPlayer1(room) || msg.sender == admin,
+            "MercuryBTTPrivateLobby: only player1 or admin can delete room"
         );
-
-        LibBidTacToe.joinGame(gameAddress, player2);
-        address aviation = burnerAddressToAviation(msg.sender);
-        gamePerPlayer[player2] = gameAddress;
-        planeMetadataPerGame[gameAddress].token2Level = getAviationLevel(player2, aviation);
-        planeMetadataPerGame[gameAddress].token2Points = getAviationPoints(player2, aviation);
-        baseJoinRoom(gameAddress, aviation);
+        mercuryBidTacToe.deleteGameInPrivateLobby(room);
+        baseJoinRoom(room);
+        baseQuitRoom(room);
     }
 
-    function deleteLobby(address lobby) external {
-        require(gameExists[lobby], "PrivateLobby: lobby does not exist");
-        require(
-            planeMetadataPerGame[lobby].token2Level == 0 && planeMetadataPerGame[lobby].token2Points == 0,
-            "PrivateLobby: the game is ongoing"
-        );
-
-        address aviation = burnerAddressToAviation(msg.sender);
-        baseJoinRoom(lobby, aviation);
-        baseQuitRoom(lobby, aviation);
-        delete gameExists[lobby];
-        delete paramsPerGame[lobby];
-        delete planeMetadataPerGame[lobby];
-        delete gamePerPlayer[msg.sender];
+    function baseCreateRoom(address newGame) private {
+        lobbyGameIndex[newGame] = lobbyGameQueue.length;
+        lobbyGameQueue.push(newGame);
     }
 
-    function baseCreateRoom(address newGame, address aviation) private {
-        lobbyGameIndex[newGame] = lobbyGameQueue[aviation].length;
-        lobbyGameQueue[aviation].push(newGame);
-    }
-
-    function baseJoinRoom(address lobby, address aviation) private {
-        address swappedRoom = lobbyGameQueue[aviation][lobbyGameQueue[aviation].length - 1];
+    function baseJoinRoom(address lobby) private {
+        address swappedRoom = lobbyGameQueue[lobbyGameQueue.length - 1];
         uint256 index = lobbyGameIndex[lobby];
-        lobbyGameQueue[aviation][index] = swappedRoom;
-        lobbyGameQueue[aviation].pop();
+        lobbyGameQueue[index] = swappedRoom;
+        lobbyGameQueue.pop();
         lobbyGameIndex[swappedRoom] = index;
         delete lobbyGameIndex[lobby];
-        lobbyOnGoingGamesIndex[lobby] = lobbyOnGoingGames[aviation].length;
-        lobbyOnGoingGames[aviation].push(lobby);
+        lobbyOnGoingGamesIndex[lobby] = lobbyOnGoingGames.length;
+        lobbyOnGoingGames.push(lobby);
     }
 
-    function baseQuitRoom(address game, address aviation) private {
-        address swappedGame = lobbyOnGoingGames[aviation][lobbyOnGoingGames[aviation].length - 1];
+    function baseQuitRoom(address game) private {
+        address swappedGame = lobbyOnGoingGames[lobbyOnGoingGames.length - 1];
         uint256 index = lobbyOnGoingGamesIndex[game];
-        lobbyOnGoingGames[aviation][index] = swappedGame;
-        lobbyOnGoingGames[aviation].pop();
+        lobbyOnGoingGames[index] = swappedGame;
+        lobbyOnGoingGames.pop();
         lobbyOnGoingGamesIndex[swappedGame] = index;
         delete lobbyOnGoingGamesIndex[game];
     }
 
-    function joinPrivateLobby() external {
-        require(!IPrivateLobbyFactory(factory).hasJoined(msg.sender), "PrivateLobby: player already joined");
+    function joinPrivateLobby() public {
         players.push(msg.sender);
-        IPrivateLobbyFactory(factory).setHasJoined(lobbyName, true);
+        mercuryBidTacToe.setActiveLobby(msg.sender);
     }
 
-    function quitPrivateLobby() external {
+    function quitPrivateLobby() external isActiveLobbyAndCorrectAviation {
         for (uint256 i = 0; i < players.length; i++) {
             if (players[i] == msg.sender) {
                 players[i] = players[players.length - 1];
                 players.pop();
             }
         }
+        mercuryBidTacToe.deactiveLobbyForPlayer(msg.sender);
         if (players.length == 0) {
-            IPrivateLobbyFactory(factory).clean(lobbyName);
-            IPrivateLobbyFactory(factory).setHasJoined(lobbyName, false);
+            mercuryBidTacToe.deletePrivateLobby(lobbyName);
         }
     }
 
     function handleWinLoss(address winnerBurner, address loserBurner) public {
-        (bool succeed,) = mercuryBidTacToe.call(abi.encodeWithSignature("handleWinLoss(address,address)", winnerBurner, loserBurner));
-        require(succeed, "PrivateLobby: handleWinLoss failed");
-        address aviation = burnerAddressToAviation(winnerBurner);
-        baseQuitRoom(msg.sender, aviation);
+        baseQuitRoom(msg.sender);
         gameHistory.push(GameHistory(winnerBurner, loserBurner));
-    }
-
-    function getAviationLevel(address burner, address aviation) internal view returns (uint64) {
-        return uint64(MercuryBase(aviation).aviationLevels(burnerAddressToTokenId(burner)));
-    }
-
-    function getAviationPoints(address burner, address aviation) internal view returns (uint64) {
-        return uint64(MercuryBase(aviation).aviationPoints(burnerAddressToTokenId(burner)));
+        winCountPerPlayer[winnerBurner] += 1;
+        loseCountPerPlayer[loserBurner] += 1;
+        mercuryBidTacToe.handleWinLoss(winnerBurner, loserBurner);
     }
 }
--- contracts/games/bidtactoe/MercuryBTTPrivateLobbyFactory.sol
+++ contracts/games/bidtactoe/MercuryBTTPrivateLobbyFactory.sol
@@ -4,36 +4,51 @@ pragma solidity ^0.8.0;
 import {MercuryBTTPrivateLobby} from "./MercuryBTTPrivateLobby.sol";
 
 contract MercuryBTTPrivateLobbyFactory {
-    mapping(string => address) public nameToPravateLobby;
-    mapping(address => bool) public hasJoined;
-    string private constant characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    mapping(address => bool) public lobbyExists;
+    mapping(string => address) public nameToPrivateLobby;
+    mapping(address => address) public activeLobbyPerPlayer;
+    string private constant characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-    function createPrivateLobby(address _mercuryBidTacToe) public returns (address) {
+    function createPrivateLobby() external returns (address) {
         string memory name = generateRandomCharacters();
-        while(nameToPravateLobby[name] != address(0)) {
+        while (nameToPrivateLobby[name] != address(0)) {
             name = generateRandomCharacters();
         }
-        MercuryBTTPrivateLobby privateLobby = new MercuryBTTPrivateLobby(_mercuryBidTacToe, name);
-        nameToPravateLobby[name] = address(privateLobby);
+
+        MercuryBTTPrivateLobby privateLobby = new MercuryBTTPrivateLobby(name, msg.sender);
+
+        nameToPrivateLobby[name] = address(privateLobby);
+        lobbyExists[address(privateLobby)] = true;
+
         return address(privateLobby);
     }
 
-    function setHasJoined(string memory name, bool joined) public {
-        require(msg.sender == nameToPravateLobby[name], "PrivateLobbyFactory: not admin");
-        hasJoined[msg.sender] = joined;
+    // Most likely private lobbies are never cleaned up, so code with that assumption
+    function deletePrivateLobby(string memory name) external {
+        require(lobbyExists[msg.sender], "MercuryBTTPrivateLobbyFactory: caller not lobby");
+        require(nameToPrivateLobby[name] == msg.sender, "MercuryBTTPrivateLobbyFactory: caller not lobby");
+        delete lobbyExists[msg.sender];
+        delete nameToPrivateLobby[name];
     }
 
-    function clean(string memory name) public {
-        require(msg.sender == nameToPravateLobby[name], "PrivateLobbyFactory: not admin");
-        delete nameToPravateLobby[name];
+    // Player management called from lobby
+    function setActiveLobby(address player) public {
+        require(lobbyExists[msg.sender], "MercuryBTTPrivateLobbyFactory: caller not lobby");
+        activeLobbyPerPlayer[player] = msg.sender;
     }
 
-    function generateRandomCharacters() public view returns (string memory) {
+    // Called from lobby; player may or may not deactive
+    function deactiveLobbyForPlayer(address player) public {
+        require(msg.sender == activeLobbyPerPlayer[player], "MercuryBTTPrivateLobbyFactory: caller not lobby");
+        delete activeLobbyPerPlayer[player];
+    }
+
+    function generateRandomCharacters() private view returns (string memory) {
         bytes memory charactersBytes = bytes(characters);
-        bytes memory result = new bytes(4);
+        bytes memory result = new bytes(6);
         uint256 random = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, gasleft())));
 
-        for (uint256 i = 0; i < 4; i++) {
+        for (uint256 i = 0; i < 6; i++) {
             result[i] = charactersBytes[random % charactersBytes.length];
             random /= charactersBytes.length;
         }
@@ -41,5 +56,3 @@ contract MercuryBTTPrivateLobbyFactory {
         return string(result);
     }
 }
-
-
