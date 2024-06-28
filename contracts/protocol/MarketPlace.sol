734bead42ea7a7bd2d1e42b5a02c97400eda969a
anonymous <anonymous>
2024-06-28 15:37:09 +0800

    update login

--- contracts/protocol/MarketPlace.sol
+++ contracts/protocol/MarketPlace.sol
@@ -10,6 +10,7 @@ contract MarketPlace {
     struct Bid {
         address bidder;
         uint256 price;
+        uint256 timestamp;
     }
 
     struct LevelInfo {
@@ -18,77 +19,104 @@ contract MarketPlace {
     }
 
     mapping(uint256 => LevelInfo) public levelInfos; // level to LevelInfo
+    mapping(address => mapping(uint256 => uint256)) public userBids; // user address => level => bid index (from 1 -> length)
 
     function bid(MercuryBase aviation, uint256 tokenId) public payable {
-        require(aviation.isApprovedOrOwner(msg.sender, tokenId), 'MarketPlace: not owner or approver');
         uint256 level = aviation.aviationLevels(tokenId);
+        require(msg.value > 0, "Bid amount must be greater than 0");
+        require(userBids[msg.sender][level] == 0, "You already have a bid for this level");
 
-        Bid memory newBid = Bid({
-            bidder: msg.sender,
-            price: msg.value
-        });
+        LevelInfo storage levelInfo = levelInfos[level];
+        Bid memory newBid = Bid(msg.sender, msg.value, block.timestamp);
 
-        Bid[] storage bidList = levelInfos[level].bids;
-        uint256 i = 0;
-        while (i < bidList.length && bidList[i].price > newBid.price) {
-            i++;
-        }
-        
-        bidList.push(newBid); // Add a dummy element to increase the length of the array
-        
-        for (uint256 j = bidList.length - 1; j > i; j--) {
-            bidList[j] = bidList[j - 1];
-        }
-        
-        bidList[i] = newBid;
+        levelInfo.bids.push(newBid);
+        userBids[msg.sender][level] = levelInfo.bids.length;
     }
 
     function cancelBid(uint256 level) public {
-        Bid[] storage bidList = levelInfos[level].bids;
-        for (uint256 i = 0; i < bidList.length; i++) {
-            if (bidList[i].bidder == msg.sender) {
-                payable(msg.sender).transfer(bidList[i].price);
-                for (uint256 j = i; j < bidList.length - 1; j++) {
-                    bidList[j] = bidList[j + 1];
-                }
-                bidList.pop();
-                break;
-            }
+        uint256 bidIndex = userBids[msg.sender][level];
+        require(bidIndex > 0, "No bid found for this level");
+
+        LevelInfo storage levelInfo = levelInfos[level];
+        uint256 actualIndex = bidIndex - 1; // Convert to 0-based index
+        uint256 lastIndex = levelInfo.bids.length - 1;
+        uint256 bidAmount = levelInfo.bids[actualIndex].price;
+
+        if (actualIndex != lastIndex) {
+            // Move the last bid to the canceled bid's position
+            levelInfo.bids[actualIndex] = levelInfo.bids[lastIndex];
+            
+            // Update the moved bid's index in userBids
+            address movedBidder = levelInfo.bids[lastIndex].bidder;
+            userBids[movedBidder][level] = bidIndex; // Keep it 1-based for userBids
         }
+        levelInfo.bids.pop();
+
+        // Reset user's bid index
+        userBids[msg.sender][level] = 0;
+
+        // Return the bid amount to the user
+        payable(msg.sender).transfer(bidAmount);
     }
 
     function sell(MercuryBase aviation, uint256 tokenId) public {
-        require(aviation.isApprovedOrOwner(msg.sender, tokenId), 'MarketPlace: not owner or approver');
         uint256 level = aviation.aviationLevels(tokenId);
+        LevelInfo storage levelInfo = levelInfos[level];
+        require(levelInfo.bids.length > 0, "No bids for this level");
 
-        Bid[] storage bidList = levelInfos[level].bids;
-        require(bidList.length > 0, "No bids available");
+        require(aviation.isApprovedOrOwner(msg.sender, tokenId), "You don't own this token");
+        require(aviation.getApproved(tokenId) == address(this), "Contract not approved for this token");
 
-        Bid memory highestBid = bidList[0];
-
-        // Transfer the plane to the highest bidder
-        aviation.transferFrom(msg.sender, highestBid.bidder, tokenId);
-
-        // Transfer the highest bid amount to the seller
-        payable(msg.sender).transfer(highestBid.price);
-
-        // Update last transacted price
-        levelInfos[level].lastTransactedPrice = highestBid.price;
+        (uint256 highestBidIndex, Bid memory highestBid) = findHighestBid(level);
+        address buyer = highestBid.bidder;
+        uint256 price = highestBid.price;
+        address owner = aviation.ownerOf(tokenId);
+        // Transfer the token
+        aviation.transferFrom(owner, buyer, tokenId);
 
         // Remove the highest bid
-        for (uint256 i = 0; i < bidList.length - 1; i++) {
-            bidList[i] = bidList[i + 1];
+        uint256 lastIndex = levelInfo.bids.length - 1;
+        if (highestBidIndex != lastIndex) {
+            // Move the last bid to the highest bid's position
+            levelInfo.bids[highestBidIndex] = levelInfo.bids[lastIndex];
+            // Update the moved bid's index in userBids
+            address movedBidder = levelInfo.bids[highestBidIndex].bidder;
+            userBids[movedBidder][level] = highestBidIndex + 1; // +1 because userBids uses 1-based index
         }
-        bidList.pop();
+        levelInfo.bids.pop();
+
+        // Reset buyer's bid index in userBids
+        userBids[buyer][level] = 0;
+
+        // Update last transacted price
+        levelInfo.lastTransactedPrice = price;
+
+        // Transfer the funds to the seller
+        payable(msg.sender).transfer(price);
+    }
+
+    function findHighestBid(uint256 level) internal view returns (uint256, Bid memory) {
+        LevelInfo storage levelInfo = levelInfos[level];
+        require(levelInfo.bids.length > 0, "No bids for this level");
+
+        uint256 highestBidIndex = 0;
+        Bid memory highestBid = levelInfo.bids[0];
+
+        for (uint256 i = 1; i < levelInfo.bids.length; i++) {
+            if (levelInfo.bids[i].price > highestBid.price || 
+               (levelInfo.bids[i].price == highestBid.price && 
+                levelInfo.bids[i].timestamp < highestBid.timestamp)) {
+                highestBid = levelInfo.bids[i];
+                highestBidIndex = i;
+            }
+        }
+
+        return (highestBidIndex, highestBid);
     }
 
     function getHighestBid(uint256 level) public view returns (uint256) {
-        Bid[] storage bidList = levelInfos[level].bids;
-        if (bidList.length == 0) {
-            return 0;
-        } else {
-            return bidList[0].price;
-        }
+        (,Bid memory highestBid) = findHighestBid(level);
+        return highestBid.price;
     }
 
     function getLastTransactedPrice(uint256 level) public view returns (uint256) {
